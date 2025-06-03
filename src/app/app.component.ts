605c10ea9b0d3f10047cd82910eab16362ecb1ea
elshenawy0 <mohamedelshnawy0@gmail.com>
2025-06-03 16:29:03 +0300

    add file of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./nav/nav.component";
-import { FooterComponent } from "./footer/footer.component";
-import { HomeComponent } from './home/home.component';
+import { NavComponent } from "./components/nav/nav.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from './components/home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, FooterComponent,HomeComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
