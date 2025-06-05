e646413615ae69f25ef61cc697e139197a5533fe
Mahmoud Elmasry <me653830@gmail.com>
2025-06-05 18:38:39 +0300

    finished last edits in home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.componente.component';
-
+import { HomeComponent } from './components/home/home.component';
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent, title: 'home' },
