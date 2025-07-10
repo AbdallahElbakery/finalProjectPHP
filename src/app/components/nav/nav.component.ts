d0ed2d17a8a0024b0a0fbe1d3be85b8a019f9181
elshenawy0 <mohamedelshnawy0@gmail.com>
2025-07-10 18:02:44 +0300

    Implement authentication system (login, register, guards, interceptor, UI integration)

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  get isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
+  get userName() {
+    return this.authService.getUserName();
+  }
+  get photo() {
+    return this.authService.getPhoto();
+  }
+  get photoUrl() {
+    const photo = this.authService.getPhoto();
+    return photo ? `http://127.0.0.1:8000/uploads/${photo}` : 'assets/images/logo.png'; // fallback to default image
+  }
+
+  logout() {
+    this.authService.logout().subscribe();
+  }
 }
