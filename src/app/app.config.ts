b8ae8e3c1b713039bbf248e93bb1b29233ddb8ee
mhmod33 <me653830@gmail.com>
2025-07-22 10:23:11 +0300

    some edits

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter,withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptorFn } from './services/auth-interceptor.service';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptors([authInterceptorFn])),
-    provideRouter(routes,withComponentInputBinding())
+    provideRouter(routes,withComponentInputBinding()), provideCharts(withDefaultRegisterables())
   ]
 };
--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -4,20 +4,19 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 import { AdminServiceService } from '../../../services/admin-service.service';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-dashboard,app-monthly-sales',
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   showSidebar: boolean = true
-
   properties: any
   users: any
   constructor(
     private adminService: AdminServiceService
   ) { }
-
+ 
   ngOnInit() {
     this.properties = this.adminService.getProperties().subscribe((res) => {
       return this.properties = res.properties
